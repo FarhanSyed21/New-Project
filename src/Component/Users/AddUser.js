8767ce30b05bada448db4f38774f492b98bc56c0
Syed Farhan <100605909+FarhanSyed21@users.noreply.github.com>
2023-07-18 16:33:28 +0530

    Update AddUser.js

--- src/Component/Users/AddUser.js
+++ src/Component/Users/AddUser.js
@@ -7,6 +7,7 @@ import ErrorModule from '../UI/ErrorModule';
 const AddUser = (props) => {
     const[enteredUserName, setEnteredUserName] = useState('');
     const[enteredAge, setEnteredAge] = useState('');
+    const[enteredCollege, setEnteredCollege] = useState('');
     const[error, setError] = useState();
 
     const userNameChangeHandler = (event) => {
@@ -15,12 +16,15 @@ const AddUser = (props) => {
     const ageChangeHandler = (event) => {
         setEnteredAge(event.target.value)
     }
+    const collegeChangeHandler = (event) => {
+        setEnteredCollege(event.target.value)
+    }
   const addUser = (event) => {
     event.preventDefault();
-    if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0){
+    if(enteredUserName.trim().length === 0 || enteredAge.trim().length === 0 || enteredCollege.trim().length === 0){
         setError({
             title: "Invalid Input",
-            message: "Please enter the name and age correctly"
+            message: "Please enter the name, age and college correctly"
         })
         return;
     }
@@ -31,9 +35,10 @@ const AddUser = (props) => {
         })
         return
     }
-    props.onAddUser(enteredUserName , enteredAge);
+    props.onAddUser(enteredUserName , enteredAge, enteredCollege);
     setEnteredUserName('')
     setEnteredAge('')
+    setEnteredCollege('')
   };
   const errorHandler = () => {
     setError(null);
@@ -48,6 +53,8 @@ const AddUser = (props) => {
             <input type="text" id="userName" value={enteredUserName} onChange={userNameChangeHandler}/>
             <label htmlFor="age">Age (Years)</label>
             <input type="number" id="age" value={enteredAge} onChange={ageChangeHandler}/>
+            <label htmlFor="college">College</label>
+            <input type="text" id="college" value={enteredCollege} onChange={collegeChangeHandler}/>
             <Button type="submit">Add User</Button>
         </form>
         </Card>
